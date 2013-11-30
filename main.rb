588b76e33b0e24e59ca1434f43a5a0ae4e182662
Mark McCraw <mark.mccraw@sas.com>
2013-11-30 09:23:45 -0500

    Adding support for discovering migrations, and tests for making sure validation is done on the command line parameters

--- main.rb
+++ main.rb
@@ -1,10 +1,34 @@
 $: << File.dirname(__FILE__)
 require "trollop"
 
+# Contains the central control logic for the migration.  Parses user input from the command line
+# and delegates tasks to other helpers.
 class Main
 
-	def self.process_params
-		opts = Trollop::options do
+	MISSING_OPTION = "Some options are missing"
+	MISSING_FILE = "could not be found.  Please make sure the file exists"
+	MISSING_MIGRATION = "No migration data for version"
+
+	# verifies that migrations can be found for the to and from versions
+	def identify_migrations
+		orig = @opts[:original_version]
+		begin
+			load "migrations/cassandra_#{orig}"
+		rescue LoadError
+			Trollop::die "#{MISSING_MIGRATION} #{orig}"
+		end
+
+		final = @opts[:final_version]
+		begin
+			load "migrations/cassandra_#{final}"
+		rescue LoadError
+			Trollop::die "#{MISSING_MIGRATION} #{final}"
+		end
+	end
+
+	# parses command line options and stores them in @opts
+	def process_params
+		@opts = Trollop::options do
 			version "cassandra_migrate version 1 (c) 2013 Steven Mark McCraw"
 			banner <<-EOS
 cassandra_migrate is a tool for migrating cassandra config files from one version to the next.
@@ -17,10 +41,19 @@ cassandra_migrate [options]
   		opt :original_version, "The version of Cassandra to migrate from", :type => String
   		opt :final_version, "The version of Cassandra to migrate to", :type => String
 		end
-		if opts[:original_version].nil? or opts[:final_config].nil? or opts[:original_version].nil? or opts[:final_version].nil?
-  		Trollop::die "Some options are missing"
+		if !@opts.include? :original_config or 
+			 !@opts.include? :final_config or 
+			 !@opts.include? :original_version or 
+			 !@opts.include? :final_version
+  		Trollop::die MISSING_OPTION
   	end
-  	Trollop::die :original_config, "must exist" unless File.exist?(opts[:original_config])
+  	Trollop::die "#{@opts[:original_config]} #{MISSING_FILE}" unless File.exist?(@opts[:original_config])
+	end
+
+	# Highest level orchestration of program flow
+	def run
+		process_params
+		identify_migrations
 	end
 
 end
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -3,14 +3,54 @@ require "main"
 
 describe Main do
 
-		describe Main, "#process_params" do
-
-			it "should exit with an appropriate message if all required options aren't given" do
+	before :all do
+		# Monkey patch an additional constructor so setup is easier for some tests
+		class Main
+			def initialize(options=nil)
+				@opts = options
 			end
-
-			it "should exit with an appropriate message if the original configuration file given doesn't exist" do
-			end
-
 		end
+	end
+
+	describe Main, ".process_params" do
+		it "should exit with an appropriate message if all required options aren't given" do
+			opts = {:original_version => "a", :final_version => "b", :original_config => "c"}
+			Trollop.should_receive(:options).and_return opts
+			Trollop.should_receive(:die).with Main::MISSING_OPTION
+
+			# for any subsequent checks, since we mocked the first one which otherwise would have exited
+			Trollop.stub :die
+			Main.new.process_params
+		end
+
+		it "should exit with an appropriate message if the original configuration file given doesn't exist" do
+			opts = {:original_version => "a", :final_version => "b", :original_config => "c", :final_config => "d"}
+			Trollop.should_receive(:options).and_return opts
+			Trollop.should_receive(:die) =~ /#{Main::MISSING_FILE}/
+			# for any subsequent checks, since we mocked the first one which otherwise would have exited
+			Trollop.stub :die 
+			Main.new.process_params
+		end
+
+		it "should return without exiting if all parameters are given and the original config file exists" do
+			opts = {:original_version => "a", :final_version => "b", :original_config => "c", :final_config => "d"}
+			Trollop.should_receive(:options).and_return opts
+			File.should_receive(:exist?).and_return true
+			Trollop.should_not_receive :die
+			Main.new.process_params
+		end
+	end
+
+	describe Main, ".identify_migrations" do
+		it "should exit with an appropriate message if migration data is missing for the source version" do
+			main = Main.new({:original_version => "13"})
+			main.should_receive(:load).with("migrations/cassandra_13").and_raise(LoadError)
+			Trollop.should_receive(:die) =~ /#{Main::MISSING_MIGRATION}/
+			# for any subsequent checks, since we mocked the first one which otherwise would have exited
+			Trollop.stub :die 
+			main.stub :load
+			main.identify_migrations
+		end
+	end
 
 end
